131f6e8086da50cae76c0dc5267a35d5ba957fe6
marnixah <marnix@hagehoeve.com>
2021-12-06 10:48:45 +0100

    feat: basic localization support

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('language/{locale}', function ($locale) {
+    app()->setLocale($locale);
+    session()->put('locale', $locale);
+    return redirect()->back();
+});
