9861e7174a52ad103e9c3cca49367a78e8491d95
marnixah <marnix@hagehoeve.com>
2021-12-09 11:23:13 +0100

    feat: MastermindGameController

---
+++ app/Http/Controllers/MastermindGameController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\MastermindDatabaseController;
+
+class MastermindGameController extends Controller
+{
+    public function create(Request $request)
+    {
+        $db = new MastermindDatabaseController();
+        $game = $db->store();
+        return redirect('/game/' . $game["id"]);
+    }
+
+    public function show(Request $request, $id)
+    {
+        $db = new MastermindDatabaseController();
+        return view('mastermind.game', ['game' => $db->get($id)]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $db = new MastermindDatabaseController();
+        $game = $db->get($id);
+        $game["board"][$game["turn"]] = $request->input('guess');
+        $db->update($game);
+        return view('mastermind.game', ['game' => $game]);
+    }
+}
