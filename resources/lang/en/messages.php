978b34372b70a4bd9e8736098cb0b4dacf7733a6
marnixah <marnix@hagehoeve.com>
2021-12-09 14:12:05 +0100

    fix: incorrect instructions from copilot
    Instructions unclear, Emoji stuck in slot

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,14 +3,9 @@
 return [
     // Mastermind game instructions
     'instructions' => <<<'EOT'
-        Mastermind is a code-breaking game. The computer has generated a secret code
-        consisting of four Emojis. The player must guess the code. The player
-        has twelve guesses to do so. Each guess is made up of four Emojis.
-        The player is awarded a point for each Emoji in the correct position. The
-        player is awarded a point for each Emoji in the correct position and color,
-        but not in the correct position. The player loses a point for each Emoji
-        in the wrong position. The player loses a point for each Emoji in the wrong
-        position and color. The player wins the game if they guess the code within
-        twelve guesses.
+        MojiMind is a code breaking game where you try to guess the correct sequence of Emojis.
+        You as the player can make up to 12 guesses. After each guess, you get hints abow how many
+        Emojis you have guessed exactly and how many are in the code, but not in the correct position.
+        The game ends when you have guessed the correct sequence or when you have run out of guesses.
     EOT
 ];
