2d7e9a42f4d2f126b05caf53e20116e2db9e2735
marnixah <marnix@hagehoeve.com>
2021-12-09 13:09:44 +0100

    fix: tune padding on instructions

--- resources/views/components/instructions.blade.php
+++ resources/views/components/instructions.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-white w-full mx-auto px-6 py-3 rounded-md shadow-xl text-gray-800 sm:max-w-xl">
+<div class="bg-white w-full mx-auto px-5 py-3 rounded-md shadow-xl text-gray-800 sm:max-w-xl">
     {{ __('messages.instructions') }}
 </div>
