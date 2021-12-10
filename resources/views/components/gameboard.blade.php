cd1235154d51dbc95f81da271e436c9c3e7b13dd
marnixah <marnix@hagehoeve.com>
2021-12-10 11:25:01 +0100

    feat: basic gameboard

---
+++ resources/views/components/gameboard.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <div class="bg-white max-w-min mx-auto my-8 grid shadow-xl px-5 py-2">
+        <h1 class="text-center text-2xl">Game {{ $game['id'] }}</h1>
+        @foreach (array_reverse($game['board']) as $row)
+            <div class="flex justify-center">
+                @foreach ($row as $emoji)
+                    {{-- Disable button if $game['turn'] == $loop->index --}}
+                    <button class="w-8 h-8 m-1 border rounded hover:bg-gray-400 disabled:opacity-50 {{ 11 - $game['turn'] !== $loop->parent->index ? 'bg-gray-200' : 'bg-gray-300' }}">
+                        {{ $emoji }}
+                    </button>
+                @endforeach
+            </div>
+        @endforeach
+    </div>
+</div>
--- resources/views/mastermind/game.blade.php
+++ resources/views/mastermind/game.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
+    <script src="{{ asset('js/app.js') }}"></script>
+    <title>{{ config('app.name') }} - game</title>
 </head>
+
 <body>
-    {{-- Dump $game as json --}}
-    <pre>
-        {{ json_encode($game, JSON_PRETTY_PRINT) }}
-    </pre>
+    <x-gameboard :game="$game" />
 </body>
