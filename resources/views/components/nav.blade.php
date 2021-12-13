1ee3d04fe3726403122f778fc947fda47b2c43d6
Daneric <danericaalbers@hotmail.com>
2021-12-13 11:24:42 +0100

    fix: colour of navbar

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,4 +1,4 @@
-<div class="sticky h-16 bg-blue-400">
+<div class="sticky h-16 bg-white shadow-md border-t-4 border-blue-700">
     <div class="h-full float-right flex items-center justify-center">
         <a class="h-full" href={{ __('shorts.changelang') }}>
             <img src={{ __('shorts.langimg') }} class="h-full p-3">
