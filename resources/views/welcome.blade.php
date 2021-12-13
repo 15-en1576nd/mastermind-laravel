549c92e9e52492c4310ad3b9a81d504a0a08d583
marnixah <marnix@hagehoeve.com>
2021-12-14 00:47:21 +0100

    fix(welcome.blade): robots

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,9 @@
 
 @section('title', __('shorts.instructions'))
 @section('description', "MojiMind is a modern take on the classic game of MasterMind with Emojis. Can you break the code in the limited number of turns?")
-@section('robots', 'index, follow')
+{{-- Don't want robots to follow links because that would result in DB
+getting spammed for no reason. (GET /game/create?difficulty= * 3) --}}
+@section('robots', 'index, nofollow')
 
 @section('content')
     <h1 class="text-6xl text-gray-800 mx-auto max-w-max my-8">{{config('app.name')}}</h1>
