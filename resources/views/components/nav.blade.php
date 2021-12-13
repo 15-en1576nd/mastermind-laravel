1d1c73cb82c915dbe519642bbdb2d3be2c81317f
Daneric <danericaalbers@hotmail.com>
2021-12-13 11:19:22 +0100

    feat: navbar

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,7 @@
+<div class="sticky h-16 bg-blue-400">
+    <div class="h-full float-right flex items-center justify-center">
+        <a class="h-full" href={{ __('shorts.changelang') }}>
+            <img src={{ __('shorts.langimg') }} class="h-full p-3">
+        </a>
+    </div>
+</div>
