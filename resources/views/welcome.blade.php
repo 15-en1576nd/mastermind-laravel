824538a495277ace572485ea5c5a2a5f44a40640
marnixah <marnix@hagehoeve.com>
2021-12-13 09:50:28 +0100

    feat(welcome.blade): difficulty picker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,11 +9,19 @@
     </head>
     <body class="antialiased">
         <h1 class="text-6xl text-gray-800 mx-auto max-w-max my-8">{{config('app.name')}}</h1>
-        <div class="flex justify-around max-w-sm mx-auto my-5">
+        <div class="flex justify-around max-w-max mx-auto my-5">
             <a
-                href="/game/create"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-semibold text-xl py-2 px-4 rounded-full"
-            >{{strtoupper(__('shorts.play'))}}</a>
+                href="/game/create?difficulty=easy"
+                class="bg-green-500 hover:bg-green-700 text-white font-semibold text-xl py-2 px-4 rounded-md mx-4"
+            >{{strtoupper(__('shorts.play'))}} EASY</a>
+            <a
+                href="/game/create?difficulty=medium"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-semibold text-xl py-2 px-4 rounded-md mx-4"
+            >{{strtoupper(__('shorts.play'))}} MEDIUM</a>
+            <a
+                href="/game/create?difficulty=hard"
+                class="bg-purple-500 hover:bg-purple-700 text-white font-semibold text-xl py-2 px-4 rounded-md mx-4"
+            >{{strtoupper(__('shorts.play'))}} HARD</a>
         </div>
         <x-instructions></x-instructions>
     </body>
