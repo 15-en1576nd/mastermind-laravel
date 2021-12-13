29f0c338378f795925616858e6f1908120657806
marnixah <marnix@hagehoeve.com>
2021-12-14 00:18:36 +0100

    refactor(emojipicker): cleaner variables

--- resources/views/components/emojipicker.blade.php
+++ resources/views/components/emojipicker.blade.php
@@ -1,17 +1,25 @@
 <div class="flex bg-white max-w-max justify-center mx-auto p-1 mb-4 shadow-xl">
-    @foreach(
-        // Inline function because have to slice of the first element
-        // This would've been a lot cleaner if the stupid props would work
-        (function() {
-            $emoji_map = app('App\Http\Controllers\SelectedEmojiController')->getEmojiMap();
-            $first_element = array_shift($emoji_map);
-            return $emoji_map;
-            })() as $emoji)
+    @php
+        $emoji_map = app('App\Http\Controllers\SelectedEmojiController')->getEmojiMap();
+        array_shift($emoji_map);
+    @endphp
+
+    @php
+        $emoji_controller = app('App\Http\Controllers\SelectedEmojiController');
+        $selected_emoji = $emoji_controller->index()['emoji_id'];
+    @endphp
+
+
+    @foreach($emoji_map as $emoji)
         <form action="/selected-emoji" method="POST">
             @csrf
             <input type="hidden" name="emoji_id" value="{{$loop->index+1}}">
             <button
-                class="w-8 h-8 m-1 border rounded {{ (app('App\Http\Controllers\SelectedEmojiController')->index()['emoji_id']-1) != $loop->index ? 'bg-gray-200 hover:bg-gray-400' : 'bg-gray-400 hover:bg-gray-600' }}"
+                @class([
+                    "w-8 h-8 m-1 border rounded",
+                    "bg-gray-200 hover:bg-gray-400" => $selected_emoji != $loop->index+1,
+                    "bg-gray-400 hover:bg-gray-600" => $selected_emoji == $loop->index+1,
+                ])
                 type="submit"
             >
                 {{ $emoji }}
