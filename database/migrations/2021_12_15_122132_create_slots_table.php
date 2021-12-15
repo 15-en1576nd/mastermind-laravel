e37f14973b7956b2d74845db317b9a0df2b7fd61
marnixah <marnix@hagehoeve.com>
2021-12-15 13:40:00 +0100

    feat: slots migration

---
+++ database/migrations/2021_12_15_122132_create_slots_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('row_id');
+            $table->integer('value');
+            $table->integer('hint');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slots');
+    }
+}
