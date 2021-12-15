fe7c175148d704b58ed59f96eff21b629c436e35
marnixah <marnix@hagehoeve.com>
2021-12-15 10:44:02 +0100

    feat: GameController

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreGameRequest;
+use App\Http\Requests\UpdateGameRequest;
+use App\Models\Game;
+
+class GameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Return a 404 error because getting all games is not allowed.
+        abort(404);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // / already has a form for creating a new game.
+        return redirect('/');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreGameRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreGameRequest $request)
+    {
+        // Create a new game.
+        $game = Game::create($request->validated());
+        // Redirect to the game's page.
+        return redirect($game->path());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Game  $game
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Game $game)
+    {
+        // Return the game's page.
+        return view('games.show', compact('game'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Game  $game
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Game $game)
+    {
+        // The games show page is the page used for editing the game.
+        return redirect($game->path());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateGameRequest  $request
+     * @param  \App\Models\Game  $game
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateGameRequest $request, Game $game)
+    {
+        // Update the game.
+        $game->update($request->validated());
+        // Redirect to the game's page.
+        return redirect($game->path());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Game  $game
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Game $game)
+    {
+        // Delete the game.
+        $game->delete();
+        // Redirect to the home page.
+        return redirect('/');
+    }
+}
