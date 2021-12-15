79629cba23e75f818ac6a949b7c38d281fe35b11
marnixah <marnix@hagehoeve.com>
2021-12-15 13:55:26 +0100

    fix(Models/Slot): missing import

--- app/Models/Slot.php
+++ app/Models/Slot.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Row;
+
 class Slot extends Model
 {
     use HasFactory;
