7c161ea1dd805f37ffa622ec01922dd4b3e52b0d
marnixah <marnix@hagehoeve.com>
2021-12-15 13:55:11 +0100

    feat(Models/Row): generateSlots after create

--- app/Models/Row.php
+++ app/Models/Row.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Game;
+use App\Models\Slot;
+
 class Row extends Model
 {
     use HasFactory;
@@ -13,6 +16,30 @@ class Row extends Model
         'game_id',
     ];
 
+    // Create x slots for each row on Row::create. x = $game->code_length.
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($row) {
+            $row->generateSlots($row->game->code_length);
+        });
+    }
+
+    // Generate x slots.
+    public function generateSlots($count)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $slot = new Slot([
+                'row_id' => $this->id,
+            ]);
+
+            $slot->save();
+        }
+    }
+
+
+    // Relations
     public function game()
     {
         return $this->belongsTo(Game::class);
