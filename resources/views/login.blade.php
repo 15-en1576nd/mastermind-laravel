515aac2c01a4176738a69ccd8e053fa046783361
marnixah <marnix@hagehoeve.com>
2021-12-17 14:21:24 +0100

    fix(views/login): show errors

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -23,12 +23,17 @@
 
             <div class="mb-3">
                 <label for="password" class="block text-gray-700 text-sm font-bold mb-2">{{ __('shorts.password') }}</label>
+                @if ($errors->has('password'))
+                    <p class="text-red-500 text-xs italic mt-4">
+                        {{ $errors->first('password') }}
+                    </p>
+                @endif
                 <input id="password" type="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('password') border-red-500 @enderror
                 " name="password" required autocomplete="new-password">
             </div>
             <div class="flex justify-around w-full">
                 <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline max-w-max">
-                    {{ __('shorts.register') }}
+                    {{ __('shorts.login') }}
                 </button>
             </div>
         </form>
