32e8b079586604d74e2056b098c0261c23a2ada6
marnixah <marnix@hagehoeve.com>
2021-12-17 14:29:37 +0100

    feat(components/nav): login btn or name

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,14 +1,22 @@
 <div class="sticky h-16 bg-white shadow-md border-t-4 border-blue-700">
     {{-- Show username if logged in --}}
-    @if (Auth::check())
-        <div class="flex justify-between items-center px-4 py-2">
-            {{-- Show username --}}
-            <p class="text-blue-700 text-lg">
-                {{ Auth::user()->name }}
-            </p>
-        </div>
-    @endif
-    <div class="h-full float-right flex items-center justify-center">
+    <div class="h-full float-right flex justify-end items-center">
+        @if (Auth::check())
+            <form action="/logout" method="POST">
+                @csrf
+                <button action="submit" class="p-2">
+                    <p class="text-blue-700 text-lg font-semibold">
+                        {{ __('shorts.welcome') }}, {{ Auth::user()->name }}
+                    </p>
+                </button>
+            </form>
+        @else
+            <a href="/login" class="p-2">
+                <p class="text-blue-700 text-lg font-semibold">
+                    {{ __('shorts.login') }}
+                </p>
+            </a>
+        @endif
         <a class="h-full" href={{ __('shorts.changelang') }}>
             <img src={{ __('shorts.langimg') }} class="h-full p-3">
         </a>
