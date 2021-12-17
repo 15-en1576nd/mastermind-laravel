26049b96a129257ee1b4deb20efaff6fc7e67455
marnixah <marnix@hagehoeve.com>
2021-12-17 14:00:22 +0100

    fix(views/login): remove password labels

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,8 +25,6 @@
                 <label for="password" class="block text-gray-700 text-sm font-bold mb-2">{{ __('shorts.password') }}</label>
                 <input id="password" type="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('password') border-red-500 @enderror
                 " name="password" required autocomplete="new-password">
-                <p class="text-gray-600 text-xs italic">{{ __('shorts.password_description') }}</p>
-                <p class="text-gray-600 text-xs italic">{{ __('shorts.password_requirements') }}</p>
             </div>
             <div class="flex justify-around w-full">
                 <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline max-w-max">
