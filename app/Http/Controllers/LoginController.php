5967f5b659aad897ae6280c5121c518c4f2b327e
marnixah <marnix@hagehoeve.com>
2021-12-17 14:21:41 +0100

    fix(LoginController): return view with errors

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -40,7 +40,7 @@ class LoginController extends Controller
             // If successful, redirect to their intended location
             return redirect()->intended('/');
         }
-        return view('login')->withErrors('Invalid credentials.');
+        return view('login')->withErrors(['password' => 'Invalid credentials.']);
     }
 
     /**
