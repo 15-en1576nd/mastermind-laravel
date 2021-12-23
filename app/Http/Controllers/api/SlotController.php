d9042f590b85b09a6f27be60ef3cb48f29e8ea1a
marnixah <marnix@hagehoeve.com>
2021-12-23 15:20:54 +0100

    fix(api/SlotController): remove unused method

--- app/Http/Controllers/api/SlotController.php
+++ app/Http/Controllers/api/SlotController.php
@@ -21,17 +21,6 @@ class SlotController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        // No need to implement this, since noone should be able to create a slot.
-        abort(404);
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreSlotRequest  $request
@@ -52,18 +41,6 @@ class SlotController extends Controller
     public function show(Slot $slot)
     {
         // No need to implement this, since noone should be able to get a slot.
-        abort(404);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Slot  $slot
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Slot $slot)
-    {
-        // No need to implement this, since the edit page is the game page.
         abort(404);
     }
 
